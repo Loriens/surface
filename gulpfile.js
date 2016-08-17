7dd799c9c0e929ee426201a3f3e824a68ad40145
Loriens <lerd.95@mail.ru>
2016-08-17 22:46:45 +0300

    Test LESS

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 
 gulp.task("less", function() {
-  gulp.src("less/style.less").
+  gulp.src("less/style.less")
     .pipe(less())
     .pipe(gulp.dest("css"));
 });
