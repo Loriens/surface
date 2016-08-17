00faf59cba904d617b52c83e89c70affae6008bb
Loriens <lerd.95@mail.ru>
2016-08-17 22:56:42 +0300

    Add script build to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 
-gulp.task("less", function() {
+gulp.task("style", function() {
   gulp.src("less/style.less")
     .pipe(less())
     .pipe(gulp.dest("css"));
